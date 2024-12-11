d6020f6d3f75e304b41caedba79a2a5a140a5bb5
cmnguyen101 <christinenguyenn101@gmail.com>
2024-12-11 21:48:43 +0000

    yes

--- classwork.cpp
+++ classwork.cpp
@@ -3,7 +3,14 @@
 
 int main()
 {
-  // write code here
+  // ( SET age TO 10)
+  int age = 10; 
+  double price = 19.99;
+  bool hasPets = true;
+
+  std::cout << age << "\n" << price << std::endl << hasPets << std::endl;
+  
+  std::cout << "I am" << age << "years old.\n";
   
   return 0;
 }
